32756bb0f77b59e64243853197c511c12121cce7
Brian <subutian@gmail.com>
2024-01-08 11:21:59 +1100

    Update App.js

--- src/containers/App.js
+++ src/containers/App.js
@@ -7,7 +7,7 @@ import "./App.css";
 function App() {
   const [robots, setRobots] = useState([]);
   const [searchfield, setSearchfield] = useState("");
-  const [count, setCount] = useState(0); // for demo purposes
+
 
   useEffect(() => {
     const fetchData = async () => {
